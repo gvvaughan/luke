5bfa155539c7fb04fd5d6e155f16c75f710df1ec
Gary V. Vaughan <gary@gnu.org>
2020-08-28 18:23:21 -0700

    spec: remove broken examples
    - at least clang requires a declaration for a called function, so remove the examples that always fail on macOS
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- spec/configure_spec.lua
+++ spec/configure_spec.lua
@@ -50,10 +50,6 @@ describe('luke.configure', function()
          assert.same(0, configure(L, env, {checkdecl='_not_a_real_c_function_decl'}))
       end)
 
-      it('finds an existing declaration', function()
-         assert.same(1, configure(L, env, {checkdecl='printf'}))
-      end)
-
       it('finds an existing declaration in given header', function()
          assert.same(1, configure(L, env, {checkdecl='gethostid', includes={'unistd.h'}}))
       end)
@@ -63,13 +59,11 @@ describe('luke.configure', function()
          local lukefile = {
             defines = {
                HAVE__NOT_A_REAL_FUNCTION_DECL = {checkdecl='_not_a_real_function_decl'},
-               HAVE_PRINTF_DECL = {checkdecl='printf'},
                HAVE_GETHOSTID_DECL = {checkdecl='gethostid', includes={'unistd.h'}},
             },
          }
          local r = run_configs(L,  env, lukefile)
          assert.same(0, r.defines.HAVE__NOT_A_REAL_FUNCTION_DECL)
-         assert.same(1, r.defines.HAVE_PRINTF_DECL)
          assert.same(1, r.defines.HAVE_GETHOSTID_DECL)
       end)
     end)
