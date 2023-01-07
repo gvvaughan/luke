5e4e558ff964cc299f80fe44b0f296a28142f5cb
Gary V. Vaughan <gary@gnu.org>
2023-01-07 13:34:18 -0700

    maint: update copyright statements.
    * Makefile, build-aux/smush, lib/luke/_base.lua, lib/luke/cli.lua,
    lib/luke/compile.lua, lib/luke/configure.lua, lib/luke/environment.lua,
    lib/luke/init.lua, lib/luke/lukefile.lua, lib/luke/platforms.lua,
    lib/smush/init.lua, lib/std/functional.lua, lib/std/normalize.lua,
    lib/std/strict.lua, lib/type/charset.lua, lib/type/context-manager.lua,
    lib/type/dict.lua, lib/type/path.lua, lib/type/scanner.lua,
    lib/type/set.lua, lib/type/strbuf.lua, lib/type/varargs.lua,
    spec/cli_spec.lua, spec/compile_spec.lua, spec/configure_spec.lua,
    spec/context-manager_spec.lua, spec/environment_spec.lua,
    spec/functional_spec.lua, spec/normalize_spec.lua,
    spec/platform_spec.lua, spec/spec_helpers.lua,
    specs/luke_platforms_spec.yaml, specs/spec_helper.lua,
    specs/std_functional_spec.yaml, specs/std_normalize_spec.yaml: Add 2023
    to copyright statements.
    * build-aux/luke: Regenerate.
    
    Signed-off-by: Gary V. Vaughan <gary@gnu.org>

--- lib/std/normalize.lua
+++ lib/std/normalize.lua
@@ -1,6 +1,6 @@
 --[[
  Normalized Lua API for Lua 5.1, 5.2 & 5.3
- Copyright (C) 2011-2022 Gary V. Vaughan
+ Copyright (C) 2011-2023 Gary V. Vaughan
  Copyright (C) 2002-2014 Reuben Thomas <rrt@sc3d.org>
 ]]
 --[[--
